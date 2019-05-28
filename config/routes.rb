e4c7224892de753c19045c4e39356009885f8c5c
kawtheressam <kawther.essam@orchtech.com>
2019-05-28 22:17:45 +0200

    add database.yml

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'validations#new'
   resources :validations
-  get '/', to: 'validations#new', as: 'index'
+  get '/', to: 'validations#new', as: 'index' 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

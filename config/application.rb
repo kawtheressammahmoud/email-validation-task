bd98734dfd7fb85bbed4d17e5c5dc852eaa6b581
kawtheressam <kawther.essam@orchtech.com>
2019-05-28 18:31:17 +0200

    gem file

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,9 @@ module Task
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
-
+    # config/application.rb
+    config.assets.initialize_on_precompile = false
+    config.assets.paths << Rails.root.join('vendor', 'assets')
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading

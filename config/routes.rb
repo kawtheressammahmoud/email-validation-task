00ebc547f678ae9a70434de35c3d60b7729dbf63
kawtheressam <kawther.essam@orchtech.com>
2019-05-28 18:23:07 +0200

    fix error

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  root 'validations#index'
+  # root 'validations#index'
   resources :validations
-  get '/', to: 'validations#index', as: 'index'
+  # get '/', to: 'validations#index', as: 'index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
